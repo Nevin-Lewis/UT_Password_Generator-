12199e40e6a40514101490ddd34175aff904857b
Nevin Lewis <lewisnevin@utexas.edu>
2022-10-03 06:07:18 -0500

    Was working on other set of files again. copied file back over. Added refresh button, and slider that resets properly

--- Assets/script.js
+++ Assets/script.js
@@ -1,13 +1,14 @@
-// Assignment Code
 var generateOpen = document.querySelector("#generate");
 var generateClose = document.querySelector("#close");
 var generatePassword = document.querySelector("#run");
 var passwordText = document.querySelector("#password");
+var retry = document.querySelector("#retry");
 generateOpen.addEventListener("click", clearform);
 generateOpen.addEventListener("click", openform);
 generateClose.addEventListener("click", closeform);
 generatePassword.addEventListener("click", writePassword);
 generatePassword.addEventListener("click", closeform);
+retry.addEventListener("click", writePassword)
 
 function openform() {
   document.getElementById("data-input").style.display = "block";
@@ -48,7 +49,8 @@ function clearform(event) {
   var clear = document.querySelectorAll("input");
 for (let i = 0; i < clear.length; i++) {
   clear[i].checked = false;}
+  document.getElementById("length").value = "8"
+  document.getElementById("results").value = "8"
+  passwordText.textContent = document.getElementById("password").ariaPlaceholder;
   password = [];
-  document.getElementById("length").value = ""
-  passwordText.textContent = password;
 }
